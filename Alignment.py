4c721a66175057e26778d75e5ca8e34b50c77b61
Nick Monzillo <nickmonzillo@gmail.com>
2015-08-07 12:27:57 -0400

    Increase compatibility between scripts

--- Alignment.py
+++ Alignment.py
@@ -1,44 +1,45 @@
 import json
-from statistics import stdev,mean
+from statistics import stdev, mean
 
 class Alignment(object):
     def __init__(self,json_fp):
         self.emails = read_json(json_fp)
-        self.raw_polarities = [x['polarity'] for x in self.emails if x['polarity'] != None]
+        self.raw_polarities = [x['polarity'] for x in self.emails if x['polarity'] != 'None']
         self.align_threshold,self.bash_threshold = self.establish_thresholds()
-        
+
     def establish_thresholds(self):
         '''Establishes the thresholds for align and bash.'''
         avg_pol = mean(self.raw_polarities)
         std_dev = stdev(self.raw_polarities)
         align = avg_pol + (0.5*std_dev)
         bash = avg_pol - (0.5*std_dev)
-        return (align,bash)
+        return align,bash
 
     def assign_alignment(self):
         '''Creates an alignment field for all emails.
         This uses the thresholds created in establish_thresholds().'''
         for email in self.emails:
-            if email['polarity'] == None:
-                email['alignment'] = None
+            if email['polarity'] == 'None':
+                email['alignment'] = 'None'
             if email['polarity'] >= self.align_threshold:
                 email['alignment'] = 'Align'
-            if email['polarity'] <= self.bash_threshold:
+            elif email['polarity'] <= self.bash_threshold:
                 email['alignment'] = 'Bash'
             else:
                 email['alignment'] = 'Neutral'
-                
-     def convert_json(self, json_path):
+    def convert_json(self,json_path):
         '''Creates a json file of email information at the specified path.'''
         with open(json_path,'w') as json_file:
             json.dump(self.emails,json_file)
-            
+
 def read_json(json_fp):
     '''Reads a json and returns the enclosed data.'''
-    with open (json_fp,'r') as json_file:
+    with open(json_fp,'r') as json_file:
         data = json.load(json_file)
         return data
 
-a = Alignment('analysis_dataset.json')
+a = Alignment('pol_test.json')
 a.assign_alignment()
-a.convert_json('analysis_dataset.json')
+print 'align: ' + str(a.align_threshold)
+print 'bash: ' + str(a.bash_threshold)
+a.convert_json('pol_test.json')
--- Analyzer.py
+++ Analyzer.py
@@ -4,20 +4,24 @@ class Analyzer(object):
     def __init__(self,json_fp):
         '''Initializes an instance of the Analyzer class.'''
         self.emails = read_json(json_fp)
-        self.data = [x for x in self.emails if x['polarity'] != None] 
-        self.total_counts = self.get_total_counts()
-        self.align_raw,self.bash_raw = self.raw_count_dicts()
+        #self.data = [x for x in self.emails if x['polarity'] != None] 
+        self.align_raw,self.bash_raw,self.total_counts = self.raw_count_dicts()
         self.align_pct = divide_dictionaries(self.total_counts,self.align_raw)
         self.bash_pct = divide_dictionaries(self.total_counts,self.bash_raw)
 
     def raw_count_dicts(self):
         '''Returns a list of dictionaries (align_dict,bash_dict)
         bash_dict[sender] = # of bash emails sent
-        align_dict[sender] = # of align emails sent'''
+        align_dict[sender] = # of align emails sent
+        total_dict[sender] = total # of emails sent'''
         align_dict = {}
         bash_dict = {}
-        for email in self.data:
+        total_dict = {}
+        for email in self.emails:
             name = email['firstname'] + ' ' + email['lastname']
+            if not total_dict.has_key(name):
+                total_dict[name] = 0
+            total_dict[name] += 1
             if not align_dict.has_key(name):
                 align_dict[name] = 0
             if not bash_dict.has_key(name):
@@ -26,26 +30,13 @@ class Analyzer(object):
                 align_dict[name] += 1
             elif email['alignment'] == 'Bash':
                 bash_dict[name] += 1
-        return (align_dict,bash_dict)
+        return (align_dict,bash_dict,total_dict)
         
-    def get_total_counts(self):
-        '''Returns a dictionary in the format total_counts['sender'] = frequency
-        where frequency is the total number of emails sent by the sender.
-        This is used to find out the alignment percentages.'''
-        total_counts = {}
-        for email in self.emails:
-            name = email['firstname'] + ' ' + email['lastname']
-            if not total_counts.has_key(name):
-                total_counts[name] = 0
-            total_counts[name] += 1
-        return total_counts
 
     def generate_csv(self):
         '''Returns the list of tuples that will be written to a csv.'''
-        senders = list(set(self.align_raw.keys()) | set(self.bash_raw.keys()))
-        #senders contains the names of all senders in self.data
         return_list = [('Sender','Total','align_raw','align_pct','bash_raw','bash_pct')]
-        for sender in senders:
+        for sender in self.total_counts.keys():
             entry = (sender,self.total_counts[sender],self.align_raw[sender],self.align_pct[sender],self.bash_raw[sender],self.bash_pct[sender])
             return_list.append(entry)
         return return_list
@@ -67,8 +58,12 @@ def write_csv(data,csv_fp):
     with open(csv_fp,'w') as out:
         csv_out=csv.writer(out,lineterminator='\n')
         for row in data:
-            csv_out.writerow(row)
+            try:
+                csv_out.writerow(row)
+            except:
+                continue
 
-a = Analyzer('analysis_dataset.json')
+a = Analyzer('pol_test.json')
 csv_list = a.generate_csv()
-write_csv(csv_list,'alignment.csv')
+print len(a.total_counts.keys())
+#write_csv(csv_list,'alignment.csv')
